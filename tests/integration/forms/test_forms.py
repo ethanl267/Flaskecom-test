028d168f08ba46a89e1311c3de46f191339510ab
ethan <ethanlesch8@gmail.com>
2021-06-09 16:16:28 +0200

    push

--- tests/integration/forms/test_forms.py
+++ tests/integration/forms/test_forms.py
@@ -1,8 +1,17 @@
 from re import U
-from tests.test_base import BaseTest, db
+
+from werkzeug.wrappers import response
+from market.tests.test_base import BaseTest, db
+from flask_login import current_user, AnonymousUserMixin
 from market.models import User
+from flask import request
 
-class RegisterForm(BaseTest):
+class SignUp(BaseTest):
     def test_username(self):
-        user = User()
-
+        with self.app:
+            response = self.app.post('/sign_up',
+                                    data=dict(username="ethanl", email_address="ethanl@gmail.com", password1=123456 , password2=123456),
+                                    follow_redirects=True)
+            user = db.session.query(User).filter_by(email='ethanl@gmail.com').first()
+            self.assertTrue(user)                       
+            self.assertIn(b'Account created', response.data)
